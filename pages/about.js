a2cbece75328d2c640b2147b2c60277a9a508dd5
kaikyou76 <kaikyou@gmail.com>
2024-06-17 18:41:54 +0800

    export defaultの削除

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Links from '../components/Links'
+import { Links } from '../components/Links'
 import { Footer } from '../components/Footer'
-import Headline from '../components/Headline'
+import { Headline } from '../components/Headline'
 
 export default function Home() {
   return (
