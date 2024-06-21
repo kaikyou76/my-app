8374c6169ed4951d290fd46f6810f350e2380465
kaikyou76 <kaikyou@gmail.com>
2024-06-21 14:12:50 +0800

    useEffect,useCallbackの第2引数の役割の勉強

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,16 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
   const[bar, setBar] = useState(1);
  
-  const handleclick = (e) =>{
-    setBar((bar) => bar + 1);
-  };
-  console.log(bar);
+  const handleclick = useCallback((e) =>{
+    console.log(bar); 
+      setBar( bar + 1);
+  },[]);
+  
   useEffect(() =>{
     document.body.style.backgroundColor = "lightblue";
     return (() =>{
